c28cac33dfcfa83b626448d61dd7e112027a3cca
AnnNavz <css.annshirleynavasca@gmail.com>
2026-02-02 17:59:03 +0800

    registers

---
+++ Models/Register.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NAVASCA_PROEL4WProject.Models
+{
+    public class Register
+    {
+        [Key]
+        public int ProfileId { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Range(16, 120, ErrorMessage = "You must be at least 16 years old")]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$",
+            ErrorMessage = "Password must contain an uppercase letter, lowercase letter, and a number")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
